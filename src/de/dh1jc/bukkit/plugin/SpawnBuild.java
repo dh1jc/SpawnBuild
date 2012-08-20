687df700cecd08519c3601059f1943274d4d680e
U-JOSCHI\jc <jc@joschi.(none)>
2012-08-20 19:25:13 +0200

    new feature for damage/fighting

--- src/de/dh1jc/bukkit/plugin/SpawnBuild.java
+++ src/de/dh1jc/bukkit/plugin/SpawnBuild.java
@@ -6,16 +6,15 @@ import de.dh1jc.bukkit.plugin.SpawnBuildListener;
  
 public class SpawnBuild extends JavaPlugin  {
 	
-	public void onEnable(){
-		getLogger().info("spawn-radius set to "+  this.getConfig().getInt("spawnbuild.spawn-radius", 10));
-		getLogger().info("Ops are ok "+  this.getConfig().getBoolean("spawnbuild.allowOp", false));
-		 
+	public void onEnable(){		 
         // save the configuration file, if there are no values, write the defaults.
         this.getConfig().options().copyDefaults(true);
         this.saveConfig();
 
 		// Create the Listener
         new SpawnBuildListener(this);    
+        getLogger().info("SpawnBuild enabled.");
+
 	}
 	
 	public void onDisable(){        
--- src/de/dh1jc/bukkit/plugin/SpawnBuildListener.java
+++ src/de/dh1jc/bukkit/plugin/SpawnBuildListener.java
@@ -4,12 +4,14 @@ import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
 
 import de.dh1jc.bukkit.plugin.SpawnBuild;
@@ -20,10 +22,22 @@ import de.dh1jc.bukkit.plugin.SpawnBuild;
 public class SpawnBuildListener implements Listener {
 	private final SpawnBuild plugin;
 
+	private final int spawnRadius;
+	private final Boolean allowDamage;
+	private final Boolean allowFight;
+	private final Boolean allowOp;
+	private final Boolean allowCreative;
+	
 	public SpawnBuildListener(SpawnBuild plugin) {
 		// Register the listener
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 		this.plugin = plugin;
+
+		this.spawnRadius = this.plugin.getConfig().getInt("spawnbuild.spawn-radius", 10);
+		this.allowOp = this.plugin.getConfig().getBoolean("spawnbuild.allowOp", false);
+		this.allowCreative = this.plugin.getConfig().getBoolean("spawnbuild.allowCreative", false);
+		this.allowDamage = this.plugin.getConfig().getBoolean("spawnbuild.allowDamage", true);
+		this.allowFight = this.plugin.getConfig().getBoolean("spawnbuild.allowFight", true);
 	}
 	
 	public Location getWorldSpawn(World world) {
@@ -39,9 +53,9 @@ public class SpawnBuildListener implements Listener {
 			Player player = event.getPlayer();
 
 			if (!hasPermissionToBuild(player)) {
-				if (getWorldSpawn(player.getWorld()).distance(player.getLocation()) <= this.plugin.getConfig().getInt("spawnbuild.spawn-radius")) {
+				if (getWorldSpawn(player.getWorld()).distance(player.getLocation()) <= spawnRadius) {
 					event.setCancelled(true);
-					player.sendMessage(ChatColor.RED + "You cannot place blocks in SpawnArea");
+					player.sendMessage(ChatColor.RED + "You cannot place blocks in SpawnArea.");
 				}
 			}
 		}
@@ -53,9 +67,9 @@ public class SpawnBuildListener implements Listener {
 			Player player = event.getPlayer();
 
 			if (!hasPermissionToBuild(player)) {
-				if (getWorldSpawn(player.getWorld()).distance(player.getLocation()) <= this.plugin.getConfig().getInt("spawnbuild.spawn-radius")) {
+				if (getWorldSpawn(player.getWorld()).distance(player.getLocation()) <= spawnRadius) {
 					event.setCancelled(true);
-					player.sendMessage(ChatColor.RED + "You cannot break blocks in SpawnArea");
+					player.sendMessage(ChatColor.RED + "You cannot break blocks in SpawnArea.");
 				}
 			}
 		}
@@ -67,18 +81,55 @@ public class SpawnBuildListener implements Listener {
 			Player player = event.getPlayer();
 
 			if (!hasPermissionToBuild(player)) {
-				if (getWorldSpawn(player.getWorld()).distance(player.getLocation()) <= this.plugin.getConfig().getInt("spawnbuild.spawn-radius", 10)) {
+				if (getWorldSpawn(player.getWorld()).distance(player.getLocation()) <= spawnRadius) {
 					event.setCancelled(true);
-					player.sendMessage(ChatColor.RED + "You cannot empty your bucket in SpawnArea");
+					player.sendMessage(ChatColor.RED + "You cannot empty your bucket in SpawnArea.");
 				}
 			}
 		}
 	}
 
+    @EventHandler (priority = EventPriority.NORMAL)
+    public void onPlayerPVP (EntityDamageByEntityEvent event) {
+        if (!event.isCancelled()) {
+            // Are you getting damaged?
+        	Entity entity = event.getEntity();
+            if (entity instanceof Player) {
+                Player player = (Player)entity;
+ 
+                if (player.hasPermission("spawnbuild.safe") || !allowDamage) {
+                	if (getWorldSpawn(player.getWorld()).distance(player.getLocation()) <= spawnRadius) {
+	                    event.setCancelled(true);
+	                    player.sendMessage(ChatColor.RED + "You cannot be damaged while in SpawnArea.");
+	
+	                    // send notify to damager
+	                    entity = event.getDamager();
+	                    if (entity instanceof Player) {
+	                    	player = (Player)entity;
+	                    	player.sendMessage(ChatColor.RED + "You cannot damage someone in SpawnArea.");
+	                    }
+	                }
+                }
+            }
+            
+            // Are you damaging others
+            entity = event.getDamager();
+            if (entity instanceof Player) {
+                Player player = (Player)entity;
+
+                if (!player.hasPermission("spawnbuild.fight") || !allowFight) {
+                	if (getWorldSpawn(player.getWorld()).distance(player.getLocation()) <= spawnRadius) {
+    	                event.setCancelled(true);
+    	                player.sendMessage(ChatColor.RED + "You cannot fight while in SpawnArea.");
+                	}
+                }
+            }
+        }
+    }
+	
 	public boolean hasPermissionToBuild(Player player) {
 		return player.hasPermission("spawnbuild") || 
-					(this.plugin.getConfig().getBoolean("spawnbuild.allowOp", true) && player.isOp()) ||
-					(this.plugin.getConfig().getBoolean("spawnbuild.allowCreative", true) && (player.getGameMode()== GameMode.CREATIVE));
+					(allowOp && player.isOp()) ||
+					(allowCreative && (player.getGameMode()== GameMode.CREATIVE));
 	}
-
 }
